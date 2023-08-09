490f4a36e32477441754a940ec549ad0f599a61e
ohs <ohs9207@gmail.com>
2023-08-09 16:17:06 +0900

    Initial commit

---
+++ src/main/java/com/example/asynchronous_practice/AsynchronousPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.asynchronous_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asynchronous_practice/AsynchronousPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asynchronous_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
